6654489b599e593269ef7dad252c844dae90d11d
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-05-12 21:52:31 +0100

    an

--- Project/animated_circle.py
+++ Project/animated_circle.py
@@ -6,4 +6,11 @@ First lets use a command line interface to create a scene the class through whic
 """
 
 # import library to draw circle : 
+from manim import * 
+
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle() # Create circle
+        circle.set_fill(PINK, opacity=0.5) # Set the color and transparency
+        self.play(Create(circle))
 
